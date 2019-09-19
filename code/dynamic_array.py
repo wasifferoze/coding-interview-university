e1bd1272d23f632dd54efc38e21b597b5d514163
wasifferoze <wasif_feroze@yahoo.com>
2019-09-19 11:50:34 +0500

    pop done: tested

--- code/dynamic_array.py
+++ code/dynamic_array.py
@@ -12,6 +12,7 @@ class DynamicArray:
     """
     class for dynamic array like built-in List type in python
     """
+
     def __init__(self):
         self.n = 0  # count actual elements in array
         self.capacity = 1  # Default capacity is 1
@@ -50,9 +51,7 @@ class DynamicArray:
         """
         Add element to end of the array
         """
-        if self.n == self.capacity:
-            # double capacity if not enough room
-            self._resize(2 * self.capacity)
+        self.inc_size()
         self.A[self.n] = element  # set element at end
         self.n += 1  # increase length count
 
@@ -62,11 +61,10 @@ class DynamicArray:
         :param index: index of array
         :param element: value
         """
-        if self.n == self.capacity:
-            self._resize(2 * self.capacity)  # double the size
-        for i in range(self.n-1, index, -1):
+        self.inc_size()
+        for i in range(self.n - 1, index, -1):
             # shift all element to right from given index
-            self.A[i+1] = self.A[i]
+            self.A[i + 1] = self.A[i]
         # insert element at given index
         self.A[index] = element
         self.n += 1
@@ -77,7 +75,7 @@ class DynamicArray:
         :param element: item to insert
         """
         if self.is_empty():
-            return IndexError('Array is empty!')
+            return IndexError('Array is empty! use append()')
         self.insert(0, element)
 
     def pop(self):
@@ -87,10 +85,20 @@ class DynamicArray:
         if self.is_empty():
             return IndexError("Array is empty")
         else:
-            self.A[self.n-1]    # slicing last element
-            self.n -= 1         # decreasing size count
-            if self.n <= 2 * self.capacity:
-                self._resize(self.capacity/2)       # reducing capacity of array
+            self.A[self.n] = self.A[self.n - 1]  # slicing last element
+            self.n -= 1  # decreasing size count
+            self.dec_size()
+
+    def delete(self, index):
+        """
+        remove item in array at given index
+        :param index:
+        :return:
+        """
+        if self.is_empty():
+            return IndexError("Array is empty! add some values")
+        for i in range(index, self.n - 1, 1):
+            self.A[i-1] = self.A[i] # TODO: have to complete this
 
     def _resize(self, new_capacity):
         """
@@ -107,6 +115,22 @@ class DynamicArray:
         check array is empty
         :return: true or false
         """
-        if self.n != 0:
-            return False
-        return True
+        return self.n == 0
+
+    def inc_size(self):
+        """
+        Increase capacity of array
+        'double the capacity if size (n) equal to capacity'
+        :return: resized array
+        """
+        if self.n == self.capacity:
+            return self._resize(2 * self.capacity)  # double the size
+
+    def dec_size(self):
+        """
+        Decrease the capacity of array
+        'formula: if size n = capacity/4 then decrease capacity to half'
+        :return: resize array
+        """
+        if self.n == int(self.capacity / 4):
+            return self._resize(int(self.capacity / 2))  # reducing capacity of array
